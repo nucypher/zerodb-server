df64c221ea152d0b59e94deabdfd6aba12d5e4d1
MacLane Wilkison <mwilkison@gmail.com>
2015-08-19 22:06:11 -0700

    Version bump

--- virtualenv.sh
+++ virtualenv.sh
@@ -8,7 +8,7 @@ then
     pip install loremipsum
     pip install names
     pushd dist
-    easy_install zerodb-0.91.1-py2.7.egg
+    easy_install zerodb-0.92-py2.7.egg
     popd
     ln -s .venv/bin/activate .
 else
