b05bd94947ecf316ef5aa04ea0042fe13f816578
Michael Egorov <michwill@gmail.com>
2016-01-26 16:13:31 +0000

    Version bump: demo error (queries with and condition) fixed in zerodb

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ INSTALL_REQUIRES = [
 
 setup(
     name="zerodb-server",
-    version="0.1.3",
+    version="0.1.4",
     description="ZeroDB server",
     author="ZeroDB Inc.",
     author_email="michael@zerodb.io",
