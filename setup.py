2418898190a5a7d637e469da051720a60e06b148
Michael Egorov <michwill@gmail.com>
2016-02-14 18:22:15 +0000

    Version bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ INSTALL_REQUIRES = [
 
 setup(
     name="zerodb-server",
-    version="0.1.7",
+    version="0.1.8",
     description="ZeroDB server",
     author="ZeroDB Inc.",
     author_email="michael@zerodb.io",
