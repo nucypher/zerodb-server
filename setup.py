cde8b9697d3b14c7c92f2ad07e42c76c9566b227
Michael Egorov <michwill@gmail.com>
2016-04-09 20:49:33 +0100

    Version bump. Be more friendly when cffi is not available

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ INSTALL_REQUIRES = [
 
 setup(
     name="zerodb-server",
-    version="0.1.19",
+    version="0.1.20",
     description="ZeroDB server",
     author="ZeroDB Inc.",
     author_email="michael@zerodb.io",
