8b7d166425b5b9e41f3085b3aceb35d7770fc5e4
Michael Egorov <michwill@gmail.com>
2016-04-24 23:55:05 +0100

    Version bump: pure-python ecc

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 INSTALL_REQUIRES = [
     'ipython>=1.0.0',
     'click',
-    'zerodb==0.97.4',
+    'zerodb==0.97.5',
 ]
 
 setup(
     name="zerodb-server",
-    version="0.1.21",
+    version="0.1.22",
     description="ZeroDB server",
     author="ZeroDB Inc.",
     author_email="michael@zerodb.io",
