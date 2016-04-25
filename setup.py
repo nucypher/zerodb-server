9c734dbf0bc8d9351847c667dd1102dc7c3fe753
Michael Egorov <michwill@gmail.com>
2016-04-25 18:32:11 +0100

    Version bump for PyPy support

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 INSTALL_REQUIRES = [
     'ipython>=1.0.0',
     'click',
-    'zerodb==0.97.5',
+    'zerodb==0.97.6',
 ]
 
 setup(
     name="zerodb-server",
-    version="0.1.22",
+    version="0.1.23",
     description="ZeroDB server",
     author="ZeroDB Inc.",
     author_email="michael@zerodb.io",
@@ -17,8 +17,7 @@ setup(
     packages=find_packages(),
     namespace_packages=["zerodbext"],
     install_requires=INSTALL_REQUIRES,
-    entry_points=
-    {
+    entry_points={
         "console_scripts":
         [
             "zerodb-server = zerodbext.server.run:run",
